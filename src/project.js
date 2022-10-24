57f899f3450bd3a4c06330f5cfaa25b45226ae9d
Jackie Yu <jackieyu247@gmail.com>
2022-10-24 15:40:26 +0100

    add functions and comments

--- src/project.js
+++ src/project.js
@@ -3,16 +3,32 @@ export default class Project{
         this.name = name;
         this.taskList = [];
     }
-
+    // GETTERS AND SETTERS //
+    setProjectName(projectName){
+        this.name = projectName;
+    }
+    getProjectName(){
+        return this.name;
+    }
+    getTaskList(){
+        return this.taskList;
+    }
+    setTaskList(newTaskList){
+        // sets this.taskList as a reference to new list;
+        this.taskList = newTaskList;
+    }
+    // METHODS //
     addTask(task){
         taskList.append(task);
     }
-
     deleteTask(task){
         var index = this.getTaskIndex(task);
         this.taskList.splice(index, 1);
     }
-
+    getTask(task){
+        return taskList[this.getTaskIndex(task)];
+    } 
+    // HELPER //
     getTaskIndex(task){
         // either returns the index or -1 if not in list
         return this.taskList.indexOf(task);
