bf8ff6b7cfca4285f35bce2ea2d3299d8abd362c
Jackie Yu <jackieyu247@gmail.com>
2022-10-28 22:08:10 +0100

    update loadprojects function

--- src/interface.js
+++ src/interface.js
@@ -1,24 +1,23 @@
 import Project from './project.js';
 import ProjectList from './projectList.js';
+import LocalStorage from './localStorage.js';
 
 export default class Interface{
-    constructor(){}
-    static displayNav(){
-
+    // METHODS //
+    static displayHome(){
+        
     }
-    static updateInterface(){
-
+    static loadProjects(){
+        LocalStorage.getSavedProjectList().getProjectList().forEach((project) => {
+            if(project.name !== 'Inbox' && project.name !== 'Today' && project.name !== 'Upcoming'){
+                Interface.createProject(project.name);
+            }
+        })
     }
     static initProjectButtons(){
         Interface.addProjectBtn();
     }
-    static loadProjects(){
-        const projectList = document.getElementById('project-list');
-        const projects = Project.getTaskList();
-        projects.array.forEach(element => {
-            
-        });
-    }
+
     static addProjectBtn(){
         const addBtn = document.getElementById('add-project-btn');
         const icon = document.getElementById('plus')
@@ -41,6 +40,39 @@ export default class Interface{
             container.appendChild(projectName);
             container.appendChild(buttons);
         });
+    }
+    // HELPER //
+    static createProject(projectName){
+        const projectList = document.getElementById('project-list');
+        const newProjectContainer = document.createElement('button');
+        const left = document.createElement('div');
+        const icon = document.createElement('i');
+        const name = document.createElement('p');
+        const right = document.createElement('div');
+        const cancel = document.createElement('i');
+
+        icon.classList.add('bi','bi-list-task');
+        newProjectContainer.classList.add('project');
+        left.classList.add('left-div');
+        right.classList.add('right-div');
+        name.classList.add('project-name');
+        cancel.classList.add('bi','bi-x-circle','project-cancel-btn');
+        name.textContent = `${projectName}`;
+
+        left.appendChild(icon);
+        left.appendChild(name);
+        right.appendChild(cancel);
+        newProjectContainer.appendChild(left);
+        newProjectContainer.appendChild(right)
+        projectList.appendChild(newProjectContainer);
+    }
+    static initProjectButton(){
+        const projectList = document.querySelectorAll('.project');
+        projectList.forEach((project) => {
+            project.addEventListener('click',() => {
+
+            })
+        })
     }
     static createAcceptButton(){
         const acceptBtn = document.createElement('button');
