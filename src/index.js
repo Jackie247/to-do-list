2467cb9e8be92a5a34b5e31170e7b388057a06b7
Jackie Yu <jackieyu247@gmail.com>
2022-11-16 15:08:15 +0000

    remove content

--- src/index.js
+++ src/index.js
@@ -3,9 +3,3 @@ import Interface from "./interface.js";
 import LocalStorage from "./localStorage";
 
 document.addEventListener('DOMContentLoaded', Interface.displayHome);
-
-const savedProjectList = LocalStorage.getSavedProjectList();
-const editForm = document.getElementById('edit-task-modal');
-editForm.addEventListener('click', e => {
-    Interface.updateTask(e,savedProjectList,editForm);
-})
