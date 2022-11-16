d678b61b383965607fc1a6c8116e0e10da4e9ecf
Jackie Yu <jackieyu247@gmail.com>
2022-11-16 15:08:01 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./style/main.scss"
 import Interface from "./interface.js";
+import LocalStorage from "./localStorage";
 
 document.addEventListener('DOMContentLoaded', Interface.displayHome);
 
-const editForm = document.getElementById('edit-task-popup');
-editForm.addEventListener('submit', e => {
-    Interface.updateTask(e,editForm);
+const savedProjectList = LocalStorage.getSavedProjectList();
+const editForm = document.getElementById('edit-task-modal');
+editForm.addEventListener('click', e => {
+    Interface.updateTask(e,savedProjectList,editForm);
 })
