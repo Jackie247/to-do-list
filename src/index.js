924f1d8a05123ba42b65bc6f5048f2313a33bb92
Jackie Yu <jackieyu247@gmail.com>
2022-11-17 15:36:59 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./style/main.scss"
 import Interface from "./interface.js";
-import LocalStorage from "./localStorage";
 
 document.addEventListener('DOMContentLoaded', Interface.displayHome);
